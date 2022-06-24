0c92761a46e324fb39c96e994bd5f6487cebeafe
Barış Can Çifçi <78167969+Bariscncfc@users.noreply.github.com>
2022-06-24 17:37:41 +0300

    Pricing
    Pricing section is created

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Process from "./components/Process";
 import Services from "./components/Services";
 import Story from "./components/Story";
 
-
 function App() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <Process />
-      <Story />
-      <Services />
-      <FAQ/>
-      <Footer/>
+      <header>
+        <Header />
+      </header>
+      <body>
+        <Hero />
+        <Process />
+        <Story />
+        <Services />
+        <FAQ />
+        <Pricing />
+      </body>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FAQ = () => {
   return (
-    <div className="mb-10" id="FAQ">
+    <div id="FAQ">
       <section className="py-12 md:py-20 px-6 md:px-0 bg-white">
         <div className="m-auto max-w-default">
           <div className="text-center">
@@ -14,7 +14,7 @@ const FAQ = () => {
           <div className="mt-10 md:mt-16">
             <div className="md:max-w-3xl m-auto">
               <details className="border-b-2">
-                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-lg focus:text-blue-700">
+                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-2xl focus:text-blue-700">
                   Will I get lifetime updates?
                 </summary>
                 <p className="py-2 px-3 text-gray-600 font-montserrat">
@@ -25,7 +25,7 @@ const FAQ = () => {
                 </p>
               </details>
               <details className="border-b-2">
-                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-lg focus:text-blue-700">
+                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-2xl focus:text-blue-700">
                   Can I use the Landify for a client's product?
                 </summary>
                 <p className="py-2 px-3 font-montserrat text-gray-600">
@@ -36,7 +36,7 @@ const FAQ = () => {
                 </p>
               </details>
               <details className="border-b-2">
-                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-lg focus:text-blue-700">
+                <summary className="p-4 text-gray-800 font-montserrat font-bold cursor-pointer pt-5 mb-1 text-2xl focus:text-blue-700">
                   Does Landify provide code kit as well?
                 </summary>
                 <p className="py-2 px-3 font-montserrat text-gray-600">
@@ -47,7 +47,7 @@ const FAQ = () => {
                 </p>
               </details>
               <details className="border-b-2">
-                <summary className="p-4 font-montserrat text-gray-800 font-bold cursor-pointer pt-5 mb-1 text-lg focus:text-blue-700">
+                <summary className="p-4 font-montserrat text-gray-800 font-bold cursor-pointer pt-5 mb-1 text-2xl focus:text-blue-700">
                   Do you have a free trial of Landify
                 </summary>
                 <p className="py-2 px-3 font-montserrat text-gray-600">
